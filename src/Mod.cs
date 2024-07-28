12f363fda2a08d2f29f7f0bef8c4ce20bc93bb09
Karol <kar.kul1993@gmail.com>
2024-07-28 14:41:06 +0200

    no message

--- src/Mod.cs
+++ src/Mod.cs
@@ -10,28 +10,28 @@
 [assembly: AssemblyDescription("Just a weapon pack")]
 
 // The mod's version
-[assembly: AssemblyVersion("1.3.0.3")]
+[assembly: AssemblyVersion("1.3.0.4")]
 
 namespace DuckGame.DairoshiMod
 {
-	public class DairoshiMod : Mod
-	{
-		// The mod's priority; this property controls the load order of the mod.
-		public override Priority priority
-		{
-			get { return base.priority; }
-		}
+    public class DairoshiMod : Mod
+    {
+        // The mod's priority; this property controls the load order of the mod.
+        public override Priority priority
+        {
+            get { return base.priority; }
+        }
 
-		// This function is run before all mods are finished loading.
-		protected override void OnPreInitialize()
-		{
-			base.OnPreInitialize();
-		}
+        // This function is run before all mods are finished loading.
+        protected override void OnPreInitialize()
+        {
+            base.OnPreInitialize();
+        }
 
-		// This function is run after all mods are loaded.
-		protected override void OnPostInitialize()
-		{
-			base.OnPostInitialize();
-		}
-	}
+        // This function is run after all mods are loaded.
+        protected override void OnPostInitialize()
+        {
+            base.OnPostInitialize();
+        }
+    }
 }
